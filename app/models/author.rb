c23bc65e49a8d4af3c5d914073048bf8fdf390ba
MaryKaheti <kahetikihara@gmail.com>
2023-06-23 14:41:06 +0300

    Completed Assignment

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,9 @@
 class Author < ApplicationRecord
+#     All authors have a name
+#    No two authors have the same name
+#   Author phone numbers are exactly ten digits
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :phone_number, length: {is: 10}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,27 @@
 class Post < ApplicationRecord
+#     All posts have a title
+#  Post content is at least 250 characters long
+#  Post summary is a maximum of 250 characters
+#  Post category is either Fiction or Non-Fiction. 
+#  This step requires an inclusion validator, which was not outlined in the lesson.
+#  You'll need to refer to the Rails guideLinks to an external site. to look up how to use it.
+
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+  validate :clickbait?
+
+  CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ]
+
+  def clickbait?
+    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
